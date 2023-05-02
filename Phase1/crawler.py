c9dede83cc18f4917bb3279529fabdbf2d711b6f
Chrystal <cbagt001@ucr.edu>
2023-05-01 20:53:35 -0700

    -added a stop based on pages crawled

--- Phase1/crawler.py
+++ Phase1/crawler.py
@@ -6,34 +6,41 @@ All crawled pages (html files) should be stored in a folder.
 Python-based Scrapy will be the default crawler used in the discussion sessions. 
 You can use other languages or libraries, like Java jsoup, but support for these will be limited.
 """
-from multiprocessing import Pool, Manager, Array, Queue, Value
+from multiprocessing import Pool, Manager, Array, Queue, Value, Lock
 from bs4 import BeautifulSoup
+from ctypes import c_int
 import requests
 import json
 import sys
+import math
 
 seed_urls = []
 maxFileSize = 100
 workers = 4
+maxHops = 6
+maxPages = math.ceil(100/workers)
+totalPagesCrawled = Value(c_int)
+MB = 0
 
 with open('seeds.txt', 'r') as seeds:
     for seed in seeds:
         seed_urls.append(str(seed.strip()))
-        
-def crawl(url, queuePool: Array, totalMBCrawled, visited_urls, outfile) -> None:
-
+     
+def crawl(url, queuePool: Array, visited_urls, outfile) -> None:
+    global totalPagesCrawled
     if(url in visited_urls):
         return
-    
-    print('fetching url', url)
+
+    #print('fetching url', url)
     html_text = requests.get(url).text
     soup = BeautifulSoup(html_text, 'lxml') 
     body = soup.body.text
 
     data = {"url": url, "body": body}
     json.dump(data, outfile)
-    # with totalMBCrawled.get_lock():
-    totalMBCrawled.value += sys.getsizeof(data) / 1048576
+    with totalPagesCrawled.get_lock():
+        totalPagesCrawled.value += 1
+    print('crawled : ', totalPagesCrawled.value)
     outfile.write('\n')
     visited_urls[url] = 0
     
@@ -49,36 +56,36 @@ def crawl(url, queuePool: Array, totalMBCrawled, visited_urls, outfile) -> None:
         for char in full_url:
             assignedQueueIndex += ord(char)
         assignedQueueIndex = assignedQueueIndex % workers
-        queuePool[assignedQueueIndex].put(full_url)
+        queuePool[assignedQueueIndex].put(full_url) 
 
-def crawler(id: int, queuePool: Array, totalMBCrawled: Value) -> None:
+
+def crawler(id: int, queuePool: Array) -> None:
     visited_urls = {}
     assignedQueue: Queue  = queuePool[id]
     outfile = open('data_' + str(id) + '.json', 'a')
     # crawl starting with given seed pages
     for url in seed_urls:
-        crawl(url, queuePool, totalMBCrawled, visited_urls, outfile)
+        crawl(url, queuePool, visited_urls, outfile)
 
-    while (totalMBCrawled.value < 500):
+    while (totalPagesCrawled.value < maxPages):
         # print(outFileSize_MB)
         url = assignedQueue.get()
-        print('inner fetched url', url)
-        crawl(url, queuePool, totalMBCrawled, visited_urls, outfile)
+        #print('inner fetched url', url)
+        crawl(url, queuePool, visited_urls, outfile)
     outfile.close()
 
 if __name__ == '__main__':
     with Pool(processes=workers) as pool:
         with Manager() as  manager:
-            totalMBCrawled = manager.Value('i', 0, lock=True)
             queuePoolArgs = []
             queuePool = [manager.Queue() for i in range(workers)]
-            poolArguments = [(i, queuePool, totalMBCrawled) for i in range(workers)]
+            poolArguments = [(i, queuePool) for i in range(workers)]
             pool.starmap(crawler, poolArguments)
 
 """
 - Still need to make it stop crawling after a certain period of time/or data limit
 - Need to account for number of pages to crawl and number of hops
-- nneds to handle duplicate pages
+- needs to handle duplicate pages
 - There might be some other smaller stuff missed or that can be improved 
 This is a very rough draft of our crawler
 """
