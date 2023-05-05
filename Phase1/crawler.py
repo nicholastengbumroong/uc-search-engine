d9bb3de1a7b900e4b4748cd57650f70f70db2c98
Nicholas Tengbumroong <nicholastengbumroong@gmail.com>
2023-05-04 21:45:04 -0700

    Integrated cmd line inputs and and script files

--- Phase1/crawler.py
+++ Phase1/crawler.py
@@ -17,19 +17,22 @@ import tldextract
 import os
 import hashlib
 
-#inputSeed = sys.argv[1]
-#inputMaxSize = sys.argv[2]
-#inputMaxHops = sys.argv[3]
-#inputOutputDir = sys.argv[4]
+# python crawler.py seeds.txt 500 100000 data 
+
+input_seed_file = sys.argv[1]
+input_max_size = sys.argv[2]
+input_max_hops = sys.argv[3]
+input_output_dir = sys.argv[4]
+
+NUM_WORKERS = 8
+MAX_HOPS = int(input_max_hops)
+TARGET_SIZE = float(input_max_size)        # in MB
+MAX_FILE_SIZE = TARGET_SIZE / NUM_WORKERS  # in MB  
 
 seed_urls = []
-workers = 8
-maxHops = 6
-TARGET_SIZE = 500        # in MB
-MAX_FILE_SIZE = TARGET_SIZE / workers  # in MB  
 
 
-with open('seeds.txt', 'r') as seeds:
+with open(input_seed_file, 'r') as seeds:
     for seed in seeds:
         seed_urls.append(str(seed.strip()))
     
@@ -46,7 +49,6 @@ def crawl(url, queuePool: Array, visited_urls, outfile, numHops, shared_fingerpr
     if(url in visited_urls):
         return
     try:
-        #print('fetching url', url)
         html_text = requests.get(url).text
         soup = BeautifulSoup(html_text, 'lxml') 
         body = soup.body.text
@@ -70,18 +72,16 @@ def crawl(url, queuePool: Array, visited_urls, outfile, numHops, shared_fingerpr
             if href.startswith('http'): # check if href is its own link
                 if sameDomainOrEdu(href, url):  # check to see if link is an edu page 
                     full_url = href
-                    #print(full_url)
                 else:
                     continue
             else:                       # else it is part of same domain
                 full_url = url + href
-                # print(full_url)
                 if not sameDomainOrEdu(full_url, url):
                     continue 
             assignedQueueIndex = 0
             for char in full_url:
                 assignedQueueIndex += ord(char)
-            assignedQueueIndex = assignedQueueIndex % workers
+            assignedQueueIndex = assignedQueueIndex % NUM_WORKERS
             queuePool[assignedQueueIndex].put((full_url, numHops)) 
     except Exception as e:
         print(e)
@@ -92,7 +92,7 @@ def crawler(id: int, queuePool: Array, shared_total_size: float, lock, shared_fi
 
     visited_urls = {}
     assignedQueue: Queue  = queuePool[id]
-    filename = 'data/data_' + str(id) + '_' + str(file_cnt) + '.json' 
+    filename = input_output_dir + '/data_' + str(id) + '_' + str(file_cnt) + '.json' 
     os.makedirs(os.path.dirname(filename), exist_ok=True)
 
     outfile = open(filename, 'w')
@@ -107,12 +107,11 @@ def crawler(id: int, queuePool: Array, shared_total_size: float, lock, shared_fi
         outfile = open(filename, 'w')
 
         while (curr_file_size < MAX_FILE_SIZE):
-            # url = assignedQueue.get()
             getQueueTuple = assignedQueue.get()
             url = getQueueTuple[0]
             numHops = getQueueTuple[1]
             # print("url: ", url, "hops :", numHops)
-            if(numHops < maxHops):
+            if(numHops < MAX_HOPS):
                 crawl(url, queuePool, visited_urls, outfile, numHops, shared_fingerprints)
 
             temp_file_size = os.path.getsize(filename) / (1024*1024.0)
@@ -137,13 +136,9 @@ def hashDoc(textBody, shared_fingerprints):
         for word in wordList: #for each word in the list
             wordSum = 0
             for char in word: #for each character in the word 
-                #print(char)
                 wordSum += ord(char) #get the ascii value of the character and sum it across the word
-            #print(wordSum)
-            #wordSum = wordSum % 65536 # no mod for 64 bits
             binWordSum = format(wordSum, '064b') #turn wordSum into binary format
             bWordList.append(binWordSum) 
-            #print(binWordSum)
         finalFingerPrint = ""
         for i in range(64): #iterates through the column of each word hashed in binary
             colSum = 0
@@ -167,11 +162,9 @@ def hashDoc(textBody, shared_fingerprints):
         finalFingerPrint = ""
         for word in wordList: #go through each word in doc
             hashedWord = hashlib.sha1(word.encode("utf-8")).hexdigest() #hash each word
-            #print('value: ', hashedWord)
             finalFingerPrint += hashedWord #concatenate hash of each word
         finalFingerPrint = hashlib.sha1(finalFingerPrint.encode("utf-8")).hexdigest() #rehash the concatenation
         finalFingerPrint = format(int(finalFingerPrint, 16), '064b')
-        #print('value: ', finalFingerPrint)
 
     if(dupeCheck(finalFingerPrint, shared_fingerprints) != -1): 
         shared_fingerprints.append(finalFingerPrint)
@@ -187,17 +180,16 @@ def dupeCheck(simHash, shared_fingerprints):
             if currHash[bit] == simHash[bit]: #if they match, increment matching bits
                 matchingBits += 1
         if(matchingBits >= 54): #if more than 54/64 bits match, return -1 to indicate its a dupe
-            #print('simhash: ', simHash, 'already there: ', currHash, 'matchingbits: ', matchingBits)
             return -1
     shared_fingerprints.append(simHash)
     return simHash
 
 if __name__ == '__main__':
-    with Pool(processes=workers) as pool:
+    with Pool(processes=NUM_WORKERS) as pool:
         with Manager() as manager:
-            queuePool = [manager.Queue() for i in range(workers)]
+            queuePool = [manager.Queue() for i in range(NUM_WORKERS)]
             shared_total_size = manager.Value('f', 0.0)
             lock = manager.Lock() 
             shared_fingerprints = manager.list()    
-            poolArguments = [(i, queuePool, shared_total_size, lock, shared_fingerprints) for i in range(workers)]
+            poolArguments = [(i, queuePool, shared_total_size, lock, shared_fingerprints) for i in range(NUM_WORKERS)]
             pool.starmap(crawler, poolArguments)
