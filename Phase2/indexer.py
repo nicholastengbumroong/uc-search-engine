1affffa7bb8e9e49c737dfa5eaed5cedfd19e6a9
Nabil <nkhal016@ucr.edu>
2023-06-02 16:08:31 -0700

    added json reader for indexer

--- Phase2/indexer.py
+++ Phase2/indexer.py
@@ -3,6 +3,7 @@ logging.disable(sys.maxsize)
 
 import lucene
 import os
+import json
 from org.apache.lucene.store import MMapDirectory, SimpleFSDirectory, NIOFSDirectory
 from java.nio.file import Paths
 from org.apache.lucene.analysis.standard import StandardAnalyzer
@@ -11,6 +12,17 @@ from org.apache.lucene.queryparser.classic import QueryParser
 from org.apache.lucene.index import FieldInfo, IndexWriter, IndexWriterConfig, IndexOptions, DirectoryReader
 from org.apache.lucene.search import IndexSearcher, BoostQuery, Query
 from org.apache.lucene.search.similarities import BM25Similarity
+
+
+documents = []
+crawled_data_path = "/home/cs172/CS172-Project/Phase2/crawled_data"
+for file in os.listdir(crawled_data_path):
+    if file.endswith("json"):
+        print(file)
+        with open(os.path.join(crawled_data_path, file)) as crawled_data_file:
+            crawled_data_json = json.load(crawled_data_file)
+            documents += crawled_data_json
+print(len(documents))
 
 sample_doc = [
     {
@@ -57,4 +69,4 @@ def create_index(dir):
     writer.close()
 
 lucene.initVM(vmargs=['-Djava.awt.headless=true'])
-create_index('sample_lucene_index/')
+#create_index('sample_lucene_index/')
